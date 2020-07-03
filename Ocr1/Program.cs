10b84fb94041807d7b28227c616d7787c8971fb3
KeepD0wn <38407409+KeepD0wn@users.noreply.github.com>
2020-07-03 14:50:08 +0300

    уменьшил время обработки, добавил таски

--- Ocr1/Program.cs
+++ Ocr1/Program.cs
@@ -4,28 +4,51 @@ using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
 using Tesseract;
 
 namespace Ocr1
 {
     class Program
     {
+        public static void PageToNumbers(Page text1, ref Queue<int> queue)
+        {
+            char[] numbrs = text1.GetText().ToCharArray();
+            int x = 0;
+            //если число, то добавляем его в лист
+            foreach (var c in numbrs)
+            {
+                if (int.TryParse(c.ToString(), out x))
+                {
+                    queue.Enqueue(Convert.ToInt32(c - '0'));
+                }
+                else
+                {
+                    //не число
+                }
+            }
+        }
+
         static void Main(string[] args)
         {
             Queue<int> answers = new Queue<int>();
-            Queue<int> question = new Queue<int>();
+            Queue<int> question = new Queue<int>();            
             PicConverter converter = new PicConverter();
             Clicker clicker = new Clicker();
-
-            Console.WriteLine("Нажмите клавишу для старта");
-            Console.ReadLine();
-
             Stopwatch sw = new Stopwatch();
+            var ocr1 = new TesseractEngine(@"./tessdata", "eng", EngineMode.TesseractAndCube);
+            var ocr2 = new TesseractEngine(@"./tessdata", "eng", EngineMode.TesseractAndCube);
+            int correctAnswer = 0;
+
+            Console.WriteLine("Введите что-нибудь для старта");
+            Console.ReadLine();
             sw.Start();
 
             try
             {
                 //берём картинку и делаем чёрно-белой
+                // метод CutImgFromScreen для скрина экрана
                 Bitmap image = new Bitmap(@"C:\Users\gvozd\source\repos\Ocr1\Ocr1\NewFolder1\image.png");
                 Bitmap imageBW = converter.BitmapToBlackWhite2(image,0.9);   
                
@@ -35,55 +58,39 @@ namespace Ocr1
                 //вырезаем вопрос и ответы
                 Bitmap questionImg = converter.CutImgFromImg(20, 8, 120, 38, imageBW, "question.png");   
                 Bitmap stackAns= converter.StackImg(imageBW);
-
-
-                //ОКР
-                var ocr = new TesseractEngine(@"./tessdata", "eng", EngineMode.TesseractAndCube);
-                var text = ocr.Process(questionImg);              
-                Console.WriteLine(text.GetText());
-
-                char[] numbrs = text.GetText().ToCharArray();
-                int x = 0;
-                //если число, то добавляем его в лист
-                foreach (var c in numbrs)
+                               
+                var task1 = Task.Factory.StartNew(() =>
                 {
-                    if (int.TryParse(c.ToString(), out x))
-                    {
-                        question.Enqueue(Convert.ToInt32(c-'0'));
-                    }
-                    else
-                    {
-                        //не число
-                    }
-                }
+                    Console.WriteLine("таск 1 начался");                    
+                    var text1 = ocr1.Process(questionImg);
+                    Console.WriteLine(text1.GetText());
 
-                int correctQuest = 0;
-                for (int i=0;i<2;i++)
+                    PageToNumbers(text1, ref question);
+                                                            
+                    for (int i = 0; i < 2; i++)
+                    {
+                        correctAnswer += question.Dequeue();
+                    }
+                    ocr1.Dispose();
+                    Console.WriteLine("таск 1 закончился");
+                });
+               
+                var task2 = Task.Factory.StartNew(() =>
                 {
-                    correctQuest += question.Dequeue();
-                }
+                    Console.WriteLine("таск 2 начался");
+                    var text2 = ocr2.Process(stackAns);
+                    Console.WriteLine(text2.GetText());
+                                       
+                    PageToNumbers(text2, ref answers);                                     
 
-                // НОВАЯ ОКР
-                text.Dispose();
-                text = ocr.Process(stackAns);
-                Console.WriteLine(text.GetText());
+                    ocr2.Dispose();
+                    Console.WriteLine("таск 2 закончился");
+                });
 
-                char[] numbrsAns = text.GetText().ToCharArray();
-                int x1 = 0;
-                foreach (var c in numbrsAns)
-                {
-                    if (int.TryParse(c.ToString(), out x1))
-                    {
-                        answers.Enqueue(Convert.ToInt32(c - '0'));
-                    }
-                    else
-                    {
-                        //не число
-                    }
-                }                
+                Task.WaitAll(task1,task2);
                 sw.Stop();
                 Console.WriteLine((sw.ElapsedMilliseconds / 1000.0).ToString()+ " секунд");
-                clicker.DoCorrectClick(answers, correctQuest);
+                clicker.DoCorrectClick(answers, correctAnswer);
             }
             catch (Exception exception)
             {
